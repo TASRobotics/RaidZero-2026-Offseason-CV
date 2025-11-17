4a4b22e50943a4224468387703a3db36d6f344a0
0x5b62656e5d <ben@benkou.dev>
2025-11-17 09:16:03 +0800

    feat: update controller bindings

--- src/main/java/raidzero/robot/RobotContainer.java
+++ src/main/java/raidzero/robot/RobotContainer.java
@@ -61,10 +61,11 @@ public class RobotContainer {
 
         joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
         joystick.b().whileTrue(
-            drivetrain.applyRequest(
-                () -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-            )
-        );
+                drivetrain.driveToNearestCoral());
+        joystick.y().whileTrue(
+                Commands.runOnce(() -> {
+                    drivetrain.getPigeon2().reset();
+                }));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
